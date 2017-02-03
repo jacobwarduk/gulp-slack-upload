9c2beecf288b4bd237d4881760f757d71319bb3f
Jacob Ward <jacob@candyboxmedia.com>
2017-02-03 17:54:51 +0000

    Add initial functionality for gulp-slack-upload

---
+++ README.md
@@ -0,0 +1,6 @@
+#gulp-slack-upload
+
+Gulp plugin to upload files to [Slack](http://slack.com/) using web hooks.
+
+##Usage
+
---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var through = require('through2');
+var Slack = require('node-slack-upload');
+
+function upload(token, config) {
+  var slack = new Slack(token);
+  
+  slack.uploadFile(config, function error(error, data) {
+    if (error) {
+      // LOG OUT ERROR
+      console.log('Error: ', error);
+    } else {
+      // LOG OUT DETAILS
+      console.log('Success: ', data);
+    }
+  });
+  
+  return through.obj(function(file, enc, callback) {
+    return callback();
+  })
+}
+
+module.exports = upload;
