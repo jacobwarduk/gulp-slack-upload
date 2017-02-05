3b0562f7d8630b46800327d0ef1b515ef0e5a768
Jacob Ward <jacob@candyboxmedia.com>
2017-02-05 08:24:04 +0000

    Fix success upload message

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function upload(token, config) {
   
   slack.uploadFile(config, function(error, data) {
     if (data && data.ok === true) {
-      gutil.log(gutil.colors.green('Success (gulp-slack-upload): Uploaded ' + data.name));
+      gutil.log(gutil.colors.green('Success (gulp-slack-upload): Uploaded ' + data.file.name));
     } else if (error) {
       gutil.log(gutil.colors.red('Error (gulp-slack-upload): ' + error));
     } else {
