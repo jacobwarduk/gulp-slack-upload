628cebde85ae9d31d261dba90eab4d90f8c87ad4
Jacob Ward <jacob@candyboxmedia.com>
2017-02-19 20:24:28 +0000

    (fix): Fix issues with logging error messages

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var through = require('through2');
 var Slack = require('node-slack-upload');
 var chalk = require('chalk');
+var gutil = require('gutil');
 var errors = require('./errors');
 
 function upload(token, config) {
@@ -10,11 +11,11 @@ function upload(token, config) {
   
   slack.uploadFile(config, function(error, data) {
     if (data && data.ok === true) {
-      chalk.green('Success (gulp-slack-upload): Uploaded ' + data.file.name);
+      gutil.log(chalk.green('Success (gulp-slack-upload): Uploaded ' + data.file.name));
     } else if (error) {
-      chalk.red('Error (gulp-slack-upload): ' + errors.hasOwnProperty(error) ? errors[error] : error);
+      gutil.log(chalk.red('Error (gulp-slack-upload): ' + (errors.hasOwnProperty(error) ? errors[error] : error)));
     } else {
-      chalk.orange('Warning (gulp-slack-upload): No valid response');
+      gutil.log(chalk.orange('Warning (gulp-slack-upload): No valid response'));
     }
   });
   
